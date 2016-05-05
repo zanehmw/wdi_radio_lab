1e81d4b60b58caec32953fecf8b6f3a8d95ec996
Zaneh Williams <zanehmw@gmail.com>
2016-05-05 15:26:13 -0400

    fixed edit and delete functions

--- js/app.js
+++ js/app.js
@@ -16,13 +16,13 @@
 
   function RouterFunction($stateProvider) {
     $stateProvider
-    .state("radioIndex", {
+    .state("songIndex", {
       url: "/songs",
       templateUrl: "js/index.html",
       controller: "RadioIndexController",
       controllerAs: "indexVm"
     })
-    .state("radioShow", {
+    .state("songShow", {
       url: "/songs/:id",
       templateUrl: "js/show.html",
       controller: "RadioShowController",
@@ -32,18 +32,35 @@
 
   RadioFactoryFunc.$inject = ["$resource"];
   function RadioFactoryFunc($resource) {
-    return $resource("http://localhost:3000/songs/:id")
+    return $resource("http://localhost:3000/songs/:id", {}, {
+      update: {method: "PUT"}
+    })
   }
 
   RadioIndexControllerFunc.$inject = ["RadioFactory"];
   function RadioIndexControllerFunc(RadioFactory) {
     var indexVm = this;
     indexVm.songs = RadioFactory.query();
+    indexVm.newSong = new RadioFactory();
+
+    indexVm.create = function($state) {
+      indexVm.newSong.$save().then(function(res) {
+        indexVm.songs.push(res)
+        indexVm.newSong = new RadioFactory();
+      })
+    };
   }
 
   RadioShowControllerFunc.$inject = [ "RadioFactory", "$stateParams"];
   function RadioShowControllerFunc(RadioFactory, $stateParams) {
     var showVm = this;
     showVm.song = RadioFactory.get({id: $stateParams.id});
-  }
+
+    showVm.update = function() {
+      showVm.song.$update({id: $stateParams.id});
+    };
+    showVm.delete = function() {
+      showVm.song.$delete({id: $stateParams.id});
+    }
+  };
 })();
