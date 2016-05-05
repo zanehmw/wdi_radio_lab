6345ef5a958215d209e764b47b065532f734b02c
Zaneh Williams <zanehmw@gmail.com>
2016-05-05 10:21:28 -0400

    made functions for the index and show controller, also made show.html and index.html files for app and tested their functionality

--- js/app.js
+++ js/app.js
@@ -18,17 +18,32 @@
     $stateProvider
     .state("radioIndex", {
       url: "/songs",
-      templateUrl: "js/index.html"
-      controller: "RadioIndexController"
+      templateUrl: "js/index.html",
+      controller: "RadioIndexController",
       controllerAs: "indexVm"
     })
     .state("radioShow", {
       url: "/songs/:id",
-      templateUrl: "js/show.html"
-      controller: "RadioShowController"
+      templateUrl: "js/show.html",
+      controller: "RadioShowController",
       controllerAs: "showVm"
     })
+  }
 
+  RadioFactoryFunc.$inject = ["$resource"];
+  function RadioFactoryFunc($resource) {
+    return $resource("http://localhost:3000/songs/:id")
+  }
 
+  RadioIndexControllerFunc.$inject = ["RadioFactory"];
+  function RadioIndexControllerFunc(RadioFactory) {
+    var indexVm = this;
+    indexVm.songs = RadioFactory.query();
+  }
+
+  RadioShowControllerFunc.$inject = [ "RadioFactory", "$stateParams"];
+  function RadioShowControllerFunc(RadioFactory, $stateParams) {
+    var showVm = this;
+    showVm.song = RadioFactory.get({id: $stateParams.id});
   }
 })();
